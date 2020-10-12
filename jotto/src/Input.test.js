998124bbfb50eef6e4809b7a974de4f155983120
Richard Chou <richard.chou@shippit.com>
2020-10-12 23:27:07 +1100

    test success reducer

--- jotto/src/Input.test.js
+++ jotto/src/Input.test.js
@@ -10,34 +10,49 @@ const setup = (initialState = {}) => {
   return wrapper;
 }
 
-setup();
-
 describe('', () => {
   describe('', () => {
-    test('', () => {
-
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
     })
 
     test('', () => {
-      
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     })
 
     test('', () => {
-      
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    })
+
+    test('', () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
     })
   });
 
   describe('', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    })
     test('', () => {
-
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
     })
 
     test('', () => {
-      
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
     })
 
     test('', () => {
-      
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
     })
   });
 });
