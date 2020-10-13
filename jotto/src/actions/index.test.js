b55c8289b4f5ad1c1f23332822ffe4f364b7ccd7
Richard Chou <richard.chou@shippit.com>
2020-10-13 23:13:09 +1100

    test redux thunk

--- jotto/src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './';
-
-describe('', () => {
-  test('', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  })
-})
