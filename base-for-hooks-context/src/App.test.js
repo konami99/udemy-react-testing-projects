c7fccf0d7cf0befa3416b8647ca9ed94c10d043c
Richard Chou <richard.chou@shippit.com>
2020-10-22 22:17:24 +1100

    spinner

--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-const setup = () => {
-  return shallow(<App />);
+import hookActions from './actions/hookActions';
+
+const mockGetSecretWord = jest.fn();
+
+const setup = (secretWord = "party") => {
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  const mockUseReducer = jest.fn().mockReturnValue([
+    { secretWord },
+    jest.fn()
+  ]);
+
+  React.useReducer = mockUseReducer;
+
+  return mount(<App />);
 }
 
 test('renders learn react link', () => {
@@ -12,3 +27,49 @@ test('renders learn react link', () => {
   const component = findByTestAttr(wrapper, 'component-app');
   expect(component.length).toBe(1);
 });
+
+describe('', () => {
+  test('', () => {
+    setup();
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  })
+  test('', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+
+    wrapper.setProps();
+    expect(mockGetSecretWord).not.toHaveBeenCalled();
+  })
+});
+
+describe('', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup("party");
+  })
+
+  test('', () => {
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.exists()).toBe(true);
+  })
+  test('', () => {
+    const appComponent = findByTestAttr(wrapper, "spinner");
+    expect(appComponent.exists()).toBe(false);
+  })
+})
+
+describe('', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup(null);
+  })
+
+  test('', () => {
+    const appComponent = findByTestAttr(wrapper, "component-app");
+    expect(appComponent.exists()).toBe(false);
+  })
+  test('', () => {
+    const appComponent = findByTestAttr(wrapper, "spinner");
+    expect(appComponent.exists()).toBe(true);
+  })
+})
