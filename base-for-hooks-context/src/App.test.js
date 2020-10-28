03d8b7de95a5119e0c678e3374fb12dbd064c613
Richard Chou <richard.chou@shippit.com>
2020-10-28 21:39:48 +1100

    GuessedWords Language context tests

--- base-for-hooks-context/src/App.test.js
+++ base-for-hooks-context/src/App.test.js
@@ -13,7 +13,7 @@ const setup = (secretWord = "party") => {
   hookActions.getSecretWord = mockGetSecretWord;
 
   const mockUseReducer = jest.fn().mockReturnValue([
-    { secretWord },
+    { secretWord, language: 'en' },
     jest.fn()
   ]);
 
